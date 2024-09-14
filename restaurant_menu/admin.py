2829345cdb4b7b1f89250052095888f1215cb47e
pavankumarm2505 <pavankumarm2505@gmail.com>
2024-09-14 13:56:11 -0400

    Full code including the admin page

--- restaurant_menu/admin.py
+++ restaurant_menu/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Item
 
-# Register your models here.
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("meal", "status", "meal_type", )
+    list_filter = ("status", )
+    search_fields = ("meals", "description",)
+
+
+admin.site.register(Item, MenuItemAdmin)
+
+# to create an admin login we have to go to the django project and want to run python manage.py createsuperuser
+# it will then ask you to create a username and then a password for now i have created using
+# username rishilshah and password as Asdfghjkl@13123
+
+# The name Items comes from the models.py
+
+
+
--- restaurant_menu/views.py
+++ restaurant_menu/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render
 from django.views import generic
-from.models import Item
+from .models import Item, Meal_type
+
 
 class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
-    def get_context_data(self):
-        context = {"meals": ["Pizza", "Pasta"],
-                   "ingredients": ["things"]
-                   }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["meals"] = Meal_type
         return context
 
 
 class MenuItemDetial(generic.DetailView):
     model = Item
     template_name = "menu_item_detail.html"
+
+
+class About(generic.TemplateView):
+    template_name = "about.html"
